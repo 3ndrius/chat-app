ba6d880e523861e5726bbea893fb533e7a792593
3ndrius <andrzejo1991@gmail.com>
2019-01-15 23:27:09 +0100

    map dummy data

--- src/components/MessageList.js
+++ src/components/MessageList.js
@@ -1,10 +1,29 @@
 import React, { Component } from 'react'
 
+const DUMMY_DATA = [
+    {
+        senderId: 'perborgen',
+        text: 'Hey, how is it going?'
+    },
+    {
+        senderId: 'janedoe',
+        text: 'Great! How about you?'
+    },
+    {
+        senderId: 'perborgen',
+        text: 'Good to hear! I am great as well'
+    }
+]
+
 export default class MessageList extends Component {
   render() {
     return (
       <div className="message-list">
-        <div className="help-text">Message List</div>
+            {DUMMY_DATA.map((message, index) => {
+                    return (
+                        <div>{message.text}</div>
+                    )
+                })}
       </div>
     )
   }
