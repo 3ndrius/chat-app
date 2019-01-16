5b69ea01c9211b4a52ad7357f6ddf70ebece49f9
3ndrius <andrzejo1991@gmail.com>
2019-01-16 20:43:54 +0100

    Adding instance locator to config

--- src/config.js
+++ src/config.js
@@ -1,4 +1,4 @@
 const tokenUrl = "https://us1.pusherplatform.io/services/chatkit_token_provider/v1/95662a3c-f796-47f8-9461-385aaf67df6b/token";
-const instanceLocator = "v1:us1:af799227-05d3-4a5b-ae27-8e378f5683b1";
+const instanceLocator = "v1:us1:95662a3c-f796-47f8-9461-385aaf67df6b";
 
 export { tokenUrl, instanceLocator }
