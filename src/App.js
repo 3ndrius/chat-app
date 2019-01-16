cbaebcfe8518b315acb085a7cfb88fdd402b8237
3ndrius <andrzejo1991@gmail.com>
2019-01-16 23:29:05 +0100

    Adding all component schema

--- src/App.js
+++ src/App.js
@@ -1,9 +1,9 @@
 import React, { Component } from 'react';
 import Chatkit from '@pusher/chatkit-client';
 import MessageList from './components/MessageList'
-// import SendMessageForm from './components/SendMessageForm'
-// import RoomList from './components/RoomList'
-// import NewRoomForm from './components/NewRoomForm'
+import SendMessageForm from './components/SendMessageForm'
+import RoomList from './components/RoomList'
+import NewRoomForm from './components/NewRoomForm'
 
 import { tokenUrl, instanceLocator } from './config'
 class App extends Component {
@@ -19,7 +19,6 @@ class App extends Component {
             url: tokenUrl
         })
     })
-  
     chatManager.connect()
     .then(currentUser => {
         currentUser.subscribeToRoom({
@@ -40,10 +39,10 @@ class App extends Component {
   render() {
     return (
       <div className="App">
-      {/* <RoomList /> */}
+      <RoomList />
       <MessageList messages={this.state.messages}/>
-      {/* <SendMessageForm />
-      <NewRoomForm /> */}
+      <SendMessageForm />
+      <NewRoomForm />
   </div>
     );
   }
