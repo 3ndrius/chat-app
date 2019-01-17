b1020345ea3fce68b883824e00eeaf5b047a6211
3ndrius <andrzejo1991@gmail.com>
2019-01-17 21:12:28 +0100

    Adding imput state send message form

--- src/components/SendMessageForm.js
+++ src/components/SendMessageForm.js
@@ -1,11 +1,23 @@
 import React, { Component } from 'react'
 
 export default class SendMessageForm extends Component {
+  state = {
+    value: ''
+  }
+  handleChange(e){
+    this.setState({
+      value: e.target.value
+    })
+  }
   render() {
     return (
-      <div>
-        
-      </div>
+      <form className="send-message-form">
+      <input
+          placeholder="SendMessageForm"
+          type="text"
+          onChange={this.handleChange} 
+          value={this.state.value}/>
+      </form>
     )
   }
 }
