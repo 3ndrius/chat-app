2fbe2e40bdd7cd0e2d6852a93544e0207f56ee58
3ndrius <andrzejo1991@gmail.com>
2019-01-24 20:29:09 +0100

    Adding scroll UX

--- src/components/MessageList.js
+++ src/components/MessageList.js
@@ -1,7 +1,19 @@
 import React, { Component } from 'react'
 import Message from './Message';
-
+import ReactDOM from 'react-dom'
 export default class MessageList extends Component {
+
+  componentWillUpdate() {
+    const node = ReactDOM.findDOMNode(this)
+    this.shouldScrollTobottom = node.scrollTop + node.clientHeight + 100 >= node.scrollHeight
+  }
+  componentDidUpdate() {
+    if(this.shouldScrollTobottom) {
+      const node = ReactDOM.findDOMNode(this)
+      node.scrollTop = node.scrollHeight
+    }
+  }
+
   render() {
     const {messages} = this.props;
     return (
